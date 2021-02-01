1de9da1451ec7dd9af5f458fd4022f1a1a0fcace
jvnsantos <jvnsantos@outlook.com>
2021-02-01 10:00:17 -0300

    Atualização 1.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# Start
+
+npm start
+
+# Obejtivo.
+
+Gerador de de numero aleatorio entre (1 a 100) utilizando:
+
+Componentes React
+States e Hooks
+Props.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import PaginaInicial from './componentes/PaginaInicial';
+
+function App() {
+  return (
+    <div className="App">
+
+      <PaginaInicial> CLICA EM MIM POXA</PaginaInicial>
+     
+    </div>
+  );
+}
+
+export default App;
